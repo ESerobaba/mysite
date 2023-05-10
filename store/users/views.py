949360dcb7c732487aad9cac2abfdc40e3968932
odinserobaba <odinserobaba@gmail.com>
2023-05-10 10:35:05 +0300

    Add auth

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
+from users.forms import UserLoginForm
 # Create your views here.
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password= password)
+            if user:
+                auth.login(request=request,user=user)
+                return HttpResponseRedirect(redirect_to=reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form' : form}
+    return render(request, 'users/login.html',context)
 
 def registration(request):
     return render(request, 'users/registration.html')
