5aee34eb21b97b2acf72161e73105b7cfbb910be
odinserobaba <odinserobaba@gmail.com>
2023-05-11 11:56:52 +0300

    add basket

--- store/products/views.py
+++ store/products/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
-from products.models import ProductCategory, Product
-
+from products.models import ProductCategory, Product, Basket
+from users.models import User
 
 def index(request):
     context = {'title': 'Store'
@@ -16,3 +16,15 @@ def products(request):
                'categories': ProductCategory.objects.all(),
                }
     return render(request, 'products/products.html', context)
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id = product_id)
+    baskets = Basket.objects.filter(user= request.user, product = product)
+    
+    if not baskets.exists():
+        Basket.objects.create(user=request.user,product=product,quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity+=1
+        basket.save()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
--- store/users/views.py
+++ store/users/views.py
@@ -3,6 +3,8 @@ from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+
+from products.models import Basket
 # Create your views here.
 
 
@@ -47,7 +49,10 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+    context = {'title': 'Store - Профиль',
+               'form': form,
+               'baskets' : Basket.objects.filter(user=request.user),
+               }
     return render(request, 'users/profile.html', context)
 
 
